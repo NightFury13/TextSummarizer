c48a38e4c2b08205be4f2f7fceadbdbd6fcfeb17
Saksham Singhal <sakshamsinghal25@gmail.com>
2015-03-24 17:53:33 +0530

    Computed Idf scores for each term

--- Preprocessing.py
+++ Preprocessing.py
@@ -2,6 +2,7 @@
 from Stemmer import Stemmer
 import sys
 import re,os
+import math
 from collections import defaultdict
 
 st = Stemmer('english')
@@ -13,6 +14,7 @@ stopWordDict = defaultdict(int)
 stopWordFile = open("./stopwords.txt","r")
 for line in stopWordFile:
 	stopWordDict[line.strip()]=1
+
 
 def extractDocumentCorpus(folder):
 	os.chdir(folder)
@@ -35,20 +37,53 @@ def extractDocumentCorpus(folder):
 	#	print fileText
 		fileptr.close()
 		if each_file not in document_to_senctence_corpus:
-			print "yes"
-			l= fileText.split(".")
-			document_to_senctence_corpus[each_file] = l
-#	print document_to_senctence_corpus.keys()
+	#		print "yes"
+	#		l= fileText.split(".")
+			document_to_senctence_corpus[each_file] = fileText
 	os.chdir("..")
 	return document_to_senctence_corpus
+
+
+def generateInverseDocFrequency(corpus):
+	total_docs = len(corpus.keys())
+	idf_scores = defaultdict(float)
+	term_doc_count = defaultdict(list)
+	for each_doc in corpus:
+		current_doc = corpus[each_doc]
+		for word in current_doc.split():
+			word = word.replace('.','')
+			if word not in stopWordDict:
+				word = st.stemWord(word)
+
+				#Checking the douments it is belonging to
+
+				if word not in term_doc_count:
+					term_doc_count[word] = [each_doc]
+				elif each_doc not in term_doc_count[word]:
+						term_doc_count[word].append(each_doc)
+
+	for term in term_doc_count:
+		idf_scores[term] = math.log10(1+ ((1.0*total_docs)/len(term_doc_count[term])))
+
+	return idf_scores	
+
+
 
 
 datasetFolder = 'DUC-2004/Cluster_of_Docs'
 os.chdir(datasetFolder)
 for cluster in os.listdir('.'):
 	document_to_senctence_corpus = extractDocumentCorpus(cluster)
-	print document_to_senctence_corpus
-	print cluster
+	idf_scores = generateInverseDocFrequency(document_to_senctence_corpus)
+
+
+	################################################################
+	#														       #
+	#			Here we need to start clustering the Docs          #
+	# 															   #	
+	################################################################
+
+
 	break
 	
 		
